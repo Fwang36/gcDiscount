fb70c6426a9f44e5bcbb61df1afeeefbd5a43834
Francis <fwang36@gmail.com>
2022-05-01 14:45:10 -0400

    add some seeds

---
+++ server/db/schema/01.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS products CASCADE;
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  image VARCHAR(255) NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png',
+  sku VARCHAR(255) NOT NULL,
+  dimensions VARCHAR(255),
+  was INTEGER,
+  price INTEGER
+)

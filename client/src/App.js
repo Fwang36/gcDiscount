b8e99c4aca692aea1ad7b25e74b8ec2f787b0df8
Francis <fwang36@gmail.com>
2022-05-05 02:24:08 -0400

    add blank text boxes template to home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,29 @@ import useApplicationData from './hooks/useApplicationData';
 import Contact from './components/Contact/Index';
 import CategoryList from './components/CategoriesList/CategoryList';
 import FeaturedItemsList from './components/FeaturedList/FeaturedItemsList';
+import TextBoxItem from './components/TextBoxes/TextBoxItem';
 
 function App() {
 
   const { state } = useApplicationData()
 
   return (
-    <div className="App">
+    <main className="App">
       <Routes>
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
+      {state.features ?
+      <FeaturedItemsList state={state} />
+      : null}
 
       {state.categories ? 
       <CategoryList state={state}/>
       : null}
 
-      {state.features ?
-      <FeaturedItemsList state={state} />
-      : null}
-    </div>
+      <TextBoxItem />
+      
+    </main>
   );
 }
 
--- client/src/components/TextBoxes/TextBoxItem.jsx
+++ client/src/components/TextBoxes/TextBoxItem.jsx
@@ -1,1 +1,23 @@
 import './TextBoxItem.scss'
+
+export default function TextBoxItem(props) {
+
+  return (
+    <section className='text-box-container'>
+      <article className='text-box-item'>
+
+
+      </article>
+
+      <article className='text-box-item'>
+
+
+      </article>
+
+      <article className='text-box-item'>
+
+        
+      </article>
+    </section>
+  )
+}
